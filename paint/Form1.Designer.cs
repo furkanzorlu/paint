79f6e5321e945d564e55cb9d771de09e3386f896
furkanzorlu <49489211+furkanzorlu@users.noreply.github.com>
2021-02-14 22:20:30 +0300

    Add files via upload

---
+++ paint/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(622, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(622, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(622, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 38);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(617, 297);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 21);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "siyah";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(617, 324);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 21);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "kırmızı";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(617, 351);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 21);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "yeşil";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(617, 378);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(53, 21);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "mor";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Location = new System.Drawing.Point(622, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 34);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Renk Seç";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(628, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(733, 198);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(55, 38);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "sil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ paint/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+      int  zaman=0;
+        Bitmap bmp = new Bitmap(640, 480);
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            
+    }
+        private Color Renk = new Color();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            Bitmap bmp = new Bitmap(pictureBox1.Width,
+            pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(Brushes.White, 0, 0, bmp.Width,
+            bmp.Height);
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+                
+        }
+
+       
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+            if (e.Button == MouseButtons.Left)
+            {
+                Bitmap bmp = (Bitmap)pictureBox1.Image;
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawEllipse(Pens.Red, e.X, e.Y, 2, 2);
+            
+                if (radioButton1.Checked)
+                {
+                    g.DrawEllipse(Pens.Black, e.X, e.Y, 2, 2);
+                }
+                else if (radioButton2.Checked)
+                {
+                    g.DrawEllipse(Pens.Red, e.X, e.Y, 2, 2);
+                }
+                else if (radioButton3.Checked)
+                {
+                    g.DrawEllipse(Pens.Green, e.X, e.Y, 2, 2);
+                }
+                else if (radioButton4.Checked)
+                {
+                    g.DrawEllipse(Pens.Purple, e.X, e.Y, 2, 2);
+                }
+                pictureBox1.Invalidate();
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                Bitmap bmp = (Bitmap)pictureBox1.Image;
+                Graphics g = Graphics.FromImage(bmp);
+               // g.DrawEllipse(Pens.White, e.X, e.Y, 2, 2);
+                g.DrawEllipse(Pens.White, e.X, e.Y,20,20);
+                
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "jpeg dosyası(*.jpg)|*.jpg|Bitmap(*.bmp)|*.bmp";
+
+            sfd.Title = "Kayıt";
+
+            sfd.FileName = "resim";
+
+            DialogResult sonuç = sfd.ShowDialog();
+
+            if (sonuç == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName);
+                MessageBox.Show("kaydedildi");
+            }
+            else
+            {
+                MessageBox.Show("kaydedilemedi");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png| Video|*.avi| Tüm Dosyalar |*.*";
+           
+            dosya.ShowDialog();
+            string DosyaYolu = dosya.FileName;
+            pictureBox1.ImageLocation = DosyaYolu;
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+
+                pictureBox1.Invalidate(); // Graphicsteki çizgileri Sil
+                Bitmap bmp = new Bitmap(pictureBox1.Width,
+           pictureBox1.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.FillRectangle(Brushes.White, 0, 0, bmp.Width,
+                bmp.Height);
+                pictureBox1.Image = bmp;
+
+
+
+            }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            radioButton3.Visible = true;
+            radioButton4.Visible = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman++;
+           
+                label1.Text = zaman.ToString() + "" + " sndir çizim yapıyosunuz..";
+            
+            
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("silmek için sağ tıka bas");
+        }
+    }
+    }
+
